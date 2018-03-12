986a4897929d4367b60ebc1fd04cae9048a38a34
Christina Brasco <christina.brasco@gmail.com>
2018-03-11 22:36:30 -0700

    add header file for Player

---
+++ Player.hpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+** Author: Christina Brasco
+** Date created: 3/11/2018
+** Last modified date: 3/11/2018
+** Description: header file for the Player class
+******************************************************************************/
+
+
+#include <string>
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player
+{
+	private:
+		std::string name;
+		bool isAutomaticPlayer;
+		int playerNumber;
+	public:
+		Player(std::string n, bool a, int n);
+		std::string getName();
+		bool isPlayerAutomatic();
+		int getPlayerNum();
+
+};
+
+#endif

988845648d9afd046913193085df25ad1e5de924
Christina Brasco <christina.brasco@gmail.com>
2018-03-11 22:35:52 -0700

    define constants

---
+++ Constants.hpp
@@ -0,0 +1,10 @@
+#include <string>
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+const int BOARD_DIM = 10;
+const int[5] SHIP_LENGTHS = {5, 4, 3, 3, 2};
+const std::string[5] SHIP_NAMES = {"Carrier","Battleship","Cruiser","Submarine","Destroyer"};
+
+#endif

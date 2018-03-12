0f95dcffaf69258b3a37840dab5f7a6bbcf854ea
Christina Brasco <christina.brasco@gmail.com>
2018-03-11 22:36:17 -0700

    update testing main
    update main to test for two-dimensional hit method

--- shipTestMain.cpp
+++ shipTestMain.cpp
@@ -6,24 +6,27 @@ using std::endl;
 
 int main()
 {
-	Ship myShip(5);
+	Ship myShip(5, 1, 1, true);
 	cout<<myShip.getSize()<<endl;
 	cout<<myShip.isShipSunk()<<endl;
 	myShip.printShip();
 
 	Ship myShip2=myShip;
 	myShip2.printShip();
-	myShip2.recordHit(0);
-	myShip2.recordHit(1);
-	myShip2.recordHit(2);
-	myShip2.recordHit(3);
-	myShip2.recordHit(4);
-	myShip.printShip();
+	myShip2.recordHit(0,0);
+	myShip2.printShip();
+	myShip2.recordHit(1, 1);
+	myShip2.printShip();
+	myShip2.recordHit(1, 2);
+	myShip2.printShip();
+	myShip2.recordHit(1, 3);
+	myShip2.printShip();
+	myShip2.recordHit(6, 1);
 	myShip2.printShip();
 	cout<<myShip.isShipSunk()<<endl;
 	cout<<myShip2.isShipSunk()<<endl;
 
-
+	void recordHit(int hitLocX, int hitLocY);
 	
 
 	return 0;

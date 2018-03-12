647031fe273dccc9f5c5efe123ce3bb14e8b8b53
Christina Brasco <christina.brasco@gmail.com>
2018-03-11 23:21:44 -0700

    add some more constants

--- Constants.hpp
+++ Constants.hpp
@@ -4,7 +4,9 @@
 #define CONSTANTS_HPP
 
 const int BOARD_DIM = 10;
+const int BOARD_SPACES = 100;
 const int[5] SHIP_LENGTHS = {5, 4, 3, 3, 2};
+const int TOTAL_SHIP_SPACES = 17;
 const std::string[5] SHIP_NAMES = {"Carrier","Battleship","Cruiser","Submarine","Destroyer"};
 
 #endif

66c6181bb2ef804952a18e07447fd26c3e41ccd7
Christina Brasco <christina.brasco@gmail.com>
2018-03-11 21:45:22 -0700

    add Board.hpp

---
+++ Board.hpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+** Author: Christina Brasco
+** Date created: 3/11/2018
+** Last modified date: 3/11/2018
+** Description: header file for the Board class
+******************************************************************************/
+
+
+#include "Ship.hpp"
+#include <vector>
+
+#ifndef SHIP_H
+#define SHIP_H
+
+class Board
+{
+	private:
+		char gameBoard[10][10]
+
+	public:
+		Board()
+		placeShip(Ship* s);
+
+};
+
+#endif

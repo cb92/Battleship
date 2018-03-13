1381b39e610ec05943e8d3b3e3de4637ea072b6b
Christina Brasco <christina.brasco@gmail.com>
2018-03-12 20:24:36 -0700

    fix constants syntax

--- Constants.hpp
+++ Constants.hpp
@@ -6,15 +6,15 @@
 const int NUM_SHIPS = 5;
 const int BOARD_DIM = 10;
 const int BOARD_SPACES = 100;
-const int[NUM_SHIPS] SHIP_LENGTHS = {5, 4, 3, 3, 2};
+const int SHIP_LENGTHS[NUM_SHIPS] = {5, 4, 3, 3, 2};
 const int TOTAL_SHIP_SPACES = 17;
-const std::string[NUM_SHIPS] SHIP_NAMES = {"Carrier","Battleship","Cruiser","Submarine","Destroyer"};
+const std::string SHIP_NAMES[NUM_SHIPS] = {"Carrier","Battleship","Cruiser","Submarine","Destroyer"};
 
 const char isHIT = 'X';
 const char isMISS = 'O';
 const char isWATER = 'w';
 const char isSAFESHIP = '_';
-const char isUNKNOWN = '?'
+const char isUNKNOWN = '?';
 
 
 #endif
